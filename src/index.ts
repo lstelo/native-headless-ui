55e5653c4a88a24a13c14f1db7d876167c18a615
raemundo <eimooamai@outlook.com>
2022-02-09 23:09:42 +0200

    feat: export in index

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { default as Avatar } from "./components/Avatar";
+export { default as Card } from "./components/Card";
+export { default as Divider } from "./components/Divider";
+export { default as Skeleton } from "./components/Skeleton";
